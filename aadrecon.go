55baba293a3993699f1961d9e37550d853e34714
yavolo <48648772+yavolo@users.noreply.github.com>
2023-11-12 15:11:28 +0100

    Add files via upload

---
+++ aadrecon.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// Structs to represent the XML structure
+type Envelope struct {
+	Body Body `xml:"Body"`
+}
+
+type Body struct {
+	Response GetFederationInformationResponse `xml:"GetFederationInformationResponseMessage"`
+}
+
+type GetFederationInformationResponse struct {
+	Response Response `xml:"Response"`
+}
+
+type Response struct {
+	Domains Domains `xml:"Domains"`
+}
+
+type Domains struct {
+	DomainList []string `xml:"Domain"`
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <domain>")
+		return
+	}
+
+	domain := os.Args[1]
+
+	url := "https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc"
+	xmlPayload := fmt.Sprintf(`<?xml version="1.0" encoding="utf-8"?>
+		<soap:Envelope xmlns:exm="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:ext="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+			<soap:Header>
+				<a:Action soap:mustUnderstand="1">http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation</a:Action>
+				<a:To soap:mustUnderstand="1">https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc</a:To>
+				<a:ReplyTo>
+					<a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+				</a:ReplyTo>
+			</soap:Header>
+			<soap:Body>
+				<GetFederationInformationRequestMessage xmlns="http://schemas.microsoft.com/exchange/2010/Autodiscover">
+					<Request>
+						<Domain>%s</Domain>
+					</Request>
+				</GetFederationInformationRequestMessage>
+			</soap:Body>
+		</soap:Envelope>`, domain)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(xmlPayload)))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "text/xml; charset=utf-8")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+	req.Header.Set("SOAPAction", "http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation")
+	req.Header.Set("User-Agent", "AutodiscoverClient")
+	req.Header.Set("Content-Length", fmt.Sprint(len(xmlPayload)))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Parse the XML response
+	var envelope Envelope
+	decoder := xml.NewDecoder(resp.Body)
+	err = decoder.Decode(&envelope)
+	if err != nil {
+		fmt.Println("Error decoding XML response:", err)
+		return
+	}
+
+	for _, domain := range envelope.Body.Response.Response.Domains.DomainList {
+		fmt.Println(domain)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,59 @@
+module aad/aadrecon
+
+go 1.21.0
+
+require (
+	github.com/Microsoft/go-winio v0.4.16 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mholt/archiver v3.1.1+incompatible // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mr-pmillz/gorecon/v2 v2.7.8 // indirect
+	github.com/nwaples/rardecode v1.1.0 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/projectdiscovery/gologger v1.1.8 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/cobra v1.6.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.13.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/ulikunitz/xz v0.5.11 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	gopkg.in/djherbis/times.v1 v1.3.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
